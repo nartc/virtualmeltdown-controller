ee66e8d10aba8ccfa74c7084d6e17eb2a10447eb
Chau Tran <ctch5@mail.umsl.edu>
2020-01-31 22:01:27 -0600

    pass name to server

--- src/app/services/colyseus-client.service.ts
+++ src/app/services/colyseus-client.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import * as Colyseus from 'colyseus.js';
-import { Observable, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { fromPromise } from 'rxjs/internal-compatibility';
 import { catchError } from 'rxjs/operators';
 import { ErrorDialogComponent } from '../layouts/error-dialog.component';
@@ -13,9 +13,14 @@ import { GameState } from '../states/GameState';
 export class ColyseusClientService {
   private _client: Colyseus.Client;
   private _room: Colyseus.Room<GameState>;
+  private _nameSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
 
   constructor(private readonly ngbModal: NgbModal) {
     this._client = new Colyseus.Client('ws://localhost:3000');
+  }
+
+  setName(name: string) {
+    this._nameSubject.next(name);
   }
 
   setRoom(room: Colyseus.Room<GameState>) {
@@ -27,6 +32,7 @@ export class ColyseusClientService {
   }
 
   join(roomId: string, username: string): Observable<Colyseus.Room<GameState>> {
+    this.setName(username);
     return fromPromise(this._client.joinById<GameState>(roomId, { username }))
       .pipe(
         catchError(err => {
@@ -39,6 +45,6 @@ export class ColyseusClientService {
   }
 
   move(vector: { x: number, y: number }) {
-    this._room.send({ vector, eventType: 'move' });
+    this._room.send({ vector, eventType: 'move', username: this._nameSubject.value });
   }
 }
