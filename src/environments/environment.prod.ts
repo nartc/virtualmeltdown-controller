97a6bf4dfdbc8da658c5af65878c85068e547a87
Chau Tran <ctch5@mail.umsl.edu>
2020-02-02 13:56:26 -0600

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serverUrl: 'ws://virtualmeltown-devserver.azurewebsites.net:80'
+  serverUrl: 'wss://virtualmeltown-devserver.azurewebsites.net:80'
 };
